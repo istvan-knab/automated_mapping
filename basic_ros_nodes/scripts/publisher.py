c5130c949d70078d451e5ee19f0bb66a714c5c94
istvan-knab <istvan.knab@icloud.com>
2023-10-08 19:13:25 +0200

    add comment

--- basic_ros_nodes/scripts/publisher.py
+++ basic_ros_nodes/scripts/publisher.py
@@ -4,9 +4,12 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
+    #this is my publisher constructor	
     pub = rospy.Publisher('topic_name', String, queue_size=10)
+    #initializing Node    
     rospy.init_node('publisher_name', anonymous=True)
     rate = rospy.Rate(1) # 1hz
+    #loop until killing Node    
     while not rospy.is_shutdown():
         string_message = "Local time: %s" % rospy.get_time()
         rospy.loginfo(string_message)
