190852a431db1109985f7af9793edb48e55bfaf6
istvan-knab <istvan.knab@icloud.com>
2023-11-10 00:27:55 +0100

    Add dumb agent

--- control/script/random_exploration.py
+++ control/script/random_exploration.py
@@ -4,6 +4,7 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 import tf2_ros
 import tf2_geometry_msgs
+from sensor_msgs.msg import LaserScan
 
 class Path_Creator:
     def __init__(self):
@@ -12,23 +13,53 @@ class Path_Creator:
         self.broadcaster = tf2_ros.TransformBroadcaster()
 
         self.path_pub = rospy.Publisher("/move_base_simple/goal",PoseStamped, queue_size=1)
-        self.timer = rospy.Timer(rospy.Duration(1), self.callback_timer)
+        #self.timer = rospy.Timer(rospy.Duration(1), self.callback_timer)
         rospy.loginfo("Starting random exploring Node")
+        self.sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback, queue_size=1)
 
     def callback_timer(self, event):
         
-        pose = PoseStamped()
-        pose.pose.position.x = 10.0
-        pose.pose.position.y = 10.0
-        pose.pose.position.z = 0.0
-        pose.pose.orientation.w = 1.0
-        if self.tfBuffer.can_transform("map", pose.header.frame_id, pose.header.stamp, rospy.Duration(0.1)):
-            # Getting the transformation
-            trans_base2map = self.tfBuffer.lookup_transform("map",pose.header.frame_id, pose.header.stamp)
+        msg = LaserScan()
+        closest_point = PoseStamped()
+        closest_point.header = msg.header
+        closest_point.pose.position.x = 10.0
+        closest_point.pose.position.y = 10.0
+        closest_point.pose.position.z = 0.0
 
-            closest_point_map = tf2_geometry_msgs.do_transform_pose(pose, trans_base2map)
+        # A quaternion nem lehet (0,0,0,0) értékű
+        closest_point.pose.orientation.w = 1
+
+        closest_point_map = PoseStamped()
+        if self.tfBuffer.can_transform("map", msg.header.frame_id, msg.header.stamp, rospy.Duration(0.1)):
+            # Getting the transformation
+            trans_base2map = self.tfBuffer.lookup_transform("map",msg.header.frame_id, msg.header.stamp)
+
+            closest_point_map = tf2_geometry_msgs.do_transform_pose(closest_point, trans_base2map)
+
+        self.path_pub.publish(closest_point_map)
+    
+    def scan_callback(self, msg: LaserScan):
+
         
-        self.path_pub.publish(pose)
+        closest_point = PoseStamped()
+        closest_point.header = msg.header
+        closest_point.pose.position.x = -0.1
+        closest_point.pose.position.y = 0.0
+        closest_point.pose.position.z = 0.0
+
+        # A quaternion nem lehet (0,0,0,0) értékű
+        closest_point.pose.orientation.w = 0.01
+
+        closest_point_map = PoseStamped()
+        if self.tfBuffer.can_transform("map", msg.header.frame_id, msg.header.stamp, rospy.Duration(0.1)):
+            # Getting the transformation
+            trans_base2map = self.tfBuffer.lookup_transform("map",msg.header.frame_id, msg.header.stamp)
+
+            closest_point_map = tf2_geometry_msgs.do_transform_pose(closest_point, trans_base2map)
+
+        self.path_pub.publish(closest_point_map)
+
+    
         
 
 
